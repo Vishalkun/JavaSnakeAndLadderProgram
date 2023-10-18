e5464c6e5ad4ea8976acff830a60e80186b36b35
Vishal github <Vishal.kunchal@gmail.com>
2023-10-18 12:24:18 +0530

    [Vishal Kunchal]:SinglePlayerAtPositionZero

---
+++ src/ladder/SinglePlayer.java
@@ -0,0 +1,23 @@
+package ladder;
+
+public class SinglePlayer
+{
+	public void start()
+	{
+		int Position=0;
+		int Playerone;
+		Playerone=Position;
+		System.out.println("Player start at zero position" +" "+ Playerone);
+	}
+	public void board()
+	{
+		start();
+	}
+
+	public static void main(String[] args) {
+		SinglePlayer sp = new SinglePlayer();
+		
+		sp.board();
+			}
+
+}
